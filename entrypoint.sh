d45da0595fcd6d98689424769d89859a8a453351
Pjay <38280665+gbpjay@users.noreply.github.com>
2022-01-26 11:51:01 +0000

    Update entrypoint.sh (#37)
    Increase heap memory to -Xmx12g and Timeout to 1200000

--- entrypoint.sh
+++ entrypoint.sh
@@ -114,8 +114,8 @@ if [ -n "${CONFIGURE_SSL}" ]; then
   sed -i "s/^\(\s*trustStorePassword\s*:\s*\).*/\1$TRUSTSTORE_PASSWORD/" agent-config.yml
 
   echo "INFO: Starting the SFT agent with SSL config..."
-  exec java -javaagent:/opt/jmx_exporter/jmx_exporter.jar=9996:/opt/jmx_exporter/jmx_exporter_config.yml -Dsun.net.client.defaultConnectTimeout=1000000 -Djavax.net.ssl.keyStore="$KEY_STORE_PATH" -Djavax.net.ssl.keyStorePassword="${KEYSTORE_PASSWORD}" -Djavax.net.ssl.trustStore="$TRUST_STORE_PATH" -Djavax.net.ssl.trustStorePassword="${TRUSTSTORE_PASSWORD}" -Djavax.net.ssl.keyAlias="${private_key_alias}" -jar -Xmx4g sft-agent.jar server agent-config.yml
+  exec java -javaagent:/opt/jmx_exporter/jmx_exporter.jar=9996:/opt/jmx_exporter/jmx_exporter_config.yml -Dsun.net.client.defaultConnectTimeout=1200000 -Djavax.net.ssl.keyStore="$KEY_STORE_PATH" -Djavax.net.ssl.keyStorePassword="${KEYSTORE_PASSWORD}" -Djavax.net.ssl.trustStore="$TRUST_STORE_PATH" -Djavax.net.ssl.trustStorePassword="${TRUSTSTORE_PASSWORD}" -Djavax.net.ssl.keyAlias="${private_key_alias}" -jar -Xmx12g sft-agent.jar server agent-config.yml
 else
   echo "INFO: Starting the SFT agent..."
-  exec java -javaagent:/opt/jmx_exporter/jmx_exporter.jar=9996:/opt/jmx_exporter/jmx_exporter_config.yml -Dsun.net.client.defaultConnectTimeout=1000000 -jar sft-agent.jar server agent-config.yml
+  exec java -javaagent:/opt/jmx_exporter/jmx_exporter.jar=9996:/opt/jmx_exporter/jmx_exporter_config.yml -Dsun.net.client.defaultConnectTimeout=1200000 -jar sft-agent.jar server agent-config.yml
 fi
