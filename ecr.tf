71b13d63f4d8fc7fde23cdc5a78d9776ea9cf02c
Camilla Scuffi <camilla.scuffi@credera.co.uk>
2022-05-12 09:55:42 +0100

    add mirror job for sft-agent-ingress

--- ecr.tf
+++ ecr.tf
@@ -16,19 +16,19 @@ output "ecr_example_url" {
 }
 
 
-resource "aws_ecr_repository" "dataworks-sft-agent" {
-  name = "dataworks-sft-agent"
+resource "aws_ecr_repository" "dataworks-sft-agent-ingress" {
+  name = "dataworks-sft-agent-ingress"
   tags = merge(
   local.common_tags,
-  { DockerHub : "dwpdigital/dataworks-sft-ingress-agent" }
+  { DockerHub : "dwpdigital/dataworks-sft-agent-ingress" }
   )
 }
 
-resource "aws_ecr_repository_policy" "dataworks-sft-agent" {
-  repository = aws_ecr_repository.dataworks-sft-agent.name
+resource "aws_ecr_repository_policy" "dataworks-sft-agent-ingress" {
+  repository = aws_ecr_repository.dataworks-sft-agent-ingress.name
   policy     = data.terraform_remote_state.management.outputs.ecr_iam_policy_document
 }
 
-output "ecr_example_url" {
-  value = aws_ecr_repository.dataworks-sft-agent.repository_url
+output "sft_agent_ingress_url" {
+  value = aws_ecr_repository.dataworks-sft-agent-ingress.repository_url
 }
