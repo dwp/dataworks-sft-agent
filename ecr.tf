754c8146a9a3bb8ca50c2db75fb23977997cb333
Camilla Scuffi <camilla.scuffi@credera.co.uk>
2022-05-12 09:30:59 +0100

    add gha and ecr for sft-agent-ingress

--- ecr.tf
+++ ecr.tf
@@ -14,3 +14,21 @@ resource "aws_ecr_repository_policy" "dataworks-sft-agent" {
 output "ecr_example_url" {
   value = aws_ecr_repository.dataworks-sft-agent.repository_url
 }
+
+
+resource "aws_ecr_repository" "dataworks-sft-agent" {
+  name = "dataworks-sft-agent"
+  tags = merge(
+  local.common_tags,
+  { DockerHub : "dwpdigital/dataworks-sft-ingress-agent" }
+  )
+}
+
+resource "aws_ecr_repository_policy" "dataworks-sft-agent" {
+  repository = aws_ecr_repository.dataworks-sft-agent.name
+  policy     = data.terraform_remote_state.management.outputs.ecr_iam_policy_document
+}
+
+output "ecr_example_url" {
+  value = aws_ecr_repository.dataworks-sft-agent.repository_url
+}
